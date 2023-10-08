b3af3410bd1674ad3485ec77866bbd8a0b9630be
Fredo <fredsotoucsm@gmail.com>
2023-10-07 19:03:23 -0500

    se cambió el compileSDK en build.gradle.kts (variable a 34)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.nasaspaceapps_proyect"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.nasaspaceapps_proyect"
