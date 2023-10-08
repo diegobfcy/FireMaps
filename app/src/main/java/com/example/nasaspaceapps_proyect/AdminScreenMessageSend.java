deea532e13c3b6f79343b1028cd45f244d9998ca
unknown <diegobfcy@gmail.com>
2023-10-07 23:58:38 -0500

    Agregadas las pantallas de mapa de mensaje de poblador y de mensaje de autoridad

---
+++ app/src/main/java/com/example/nasaspaceapps_proyect/AdminScreenMessageSend.java
@@ -0,0 +1,14 @@
+package com.example.nasaspaceapps_proyect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminScreenMessageSend extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_screen_message_send);
+    }
+}
---
+++ app/src/main/java/com/example/nasaspaceapps_proyect/UserScreenMessage.java
@@ -0,0 +1,14 @@
+package com.example.nasaspaceapps_proyect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserScreenMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_screen_message);
+    }
+}
